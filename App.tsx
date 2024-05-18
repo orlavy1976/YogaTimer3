4efcc37700aa688f926d2cf40fc44a457b257e98
Or Layv <or@atera.com>
2024-05-18 21:17:43 +0300

    add keep awake

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
+import { useKeepAwake } from 'expo-keep-awake';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { GlobalProvider } from './context/GlobalProvider';
 import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
+  useKeepAwake();
 
   return <GestureHandlerRootView style={{ flex: 1 }}>
     <GlobalProvider>
