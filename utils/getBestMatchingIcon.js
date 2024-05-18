6fac3897c5dc6da3317373b56a35f2ec15fb5bec
Or Layv <or@atera.com>
2024-05-18 22:49:48 +0300

    add flow icon

--- utils/getBestMatchingIcon.js
+++ utils/getBestMatchingIcon.js
@@ -2,6 +2,7 @@ import { getLevenshteinSimilarity } from './levenshtein';
 
 const icons = [
   { name: 'asana', src: require('../assets/icons/asana.jpg') },
+  { name: 'flow', tags: ['long flow'], src: require('../assets/icons/flow.jpg') },
   { name: 'bliss', src: require('../assets/icons/bliss.jpg') },
   { name: 'chakrasana', src: require('../assets/icons/chakrasana.jpg') },
   { name: 'meditation', src: require('../assets/icons/meditation.jpg') },
@@ -16,7 +17,15 @@ const getBestMatchingIcon = (exerciseName) => {
   let highestSimilarity = 0;
 
   icons.forEach(icon => {
-    const similarity = getLevenshteinSimilarity(exerciseName, icon.name);
+    let similarity = getLevenshteinSimilarity(exerciseName, icon.name);
+    if (icon.tags) {
+      icon.tags.forEach(tag => {
+        const tagSimilarity = getLevenshteinSimilarity(exerciseName, tag);
+        if (tagSimilarity > similarity) {
+          similarity = tagSimilarity;
+        }
+      });
+    }
     if (similarity > highestSimilarity) {
       highestSimilarity = similarity;
       bestMatch = icon.src;
