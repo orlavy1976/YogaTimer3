418b2cc9ac5965cfe73da2386308afec164f3089
Or Layv <or@atera.com>
2024-05-29 09:26:49 +0300

    status bar fixes and keep awake

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { useKeepAwake } from 'expo-keep-awake';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { GlobalProvider } from './context/GlobalProvider';
 import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
-  useKeepAwake();
+  useEffect(() => {
+    useKeepAwake();
+  }, []);
 
   return <GestureHandlerRootView style={{ flex: 1 }}>
     <GlobalProvider>
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import ExerciseDetails from '../components/ExerciseDetails';
 import ExerciseList from '../components/ExerciseList';
 import TimerDetails from '../components/TimerDetails';
@@ -10,15 +12,25 @@ const Stack = createNativeStackNavigator();
 export default function StackNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTintColor: colors.primary,
-          headerStyle: { backgroundColor: '#fffde7' },
-        }}>
-        <Stack.Screen name="ExerciseList" component={ExerciseList} options={{ title: 'Yoga Timer' }} />
-        <Stack.Screen name="ExerciseDetails" component={ExerciseDetails} options={{ title: 'Exercise Details' }} />
-        <Stack.Screen name="TimerDetails" component={TimerDetails} options={{ title: 'Timer Details' }} />
-      </Stack.Navigator>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Stack.Navigator
+          screenOptions={{
+            headerTintColor: colors.primary,
+            headerStyle: { backgroundColor: '#fffde7' },
+          }}>
+          <Stack.Screen name="ExerciseList" component={ExerciseList} options={{ title: 'Yoga Timer' }} />
+          <Stack.Screen name="ExerciseDetails" component={ExerciseDetails} options={{ title: 'Exercise Details' }} />
+          <Stack.Screen name="TimerDetails" component={TimerDetails} options={{ title: 'Timer Details' }} />
+        </Stack.Navigator>
+      </View>
+
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
