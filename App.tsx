fdedb45e0201ef1fb74cf5a6db6234e1a7839802
Or Layv <or@atera.com>
2024-05-29 09:36:52 +0300

    roll back keep awake changes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { useKeepAwake } from 'expo-keep-awake';
-import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { GlobalProvider } from './context/GlobalProvider';
 import StackNavigator from './navigation/StackNavigator';
 
-export default function App() {
-  useEffect(() => {
-    useKeepAwake();
-  }, []);
 
+export default function App() {
+  useKeepAwake();
   return <GestureHandlerRootView style={{ flex: 1 }}>
     <GlobalProvider>
       <StackNavigator />
